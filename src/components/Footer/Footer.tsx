5b7832e403dcf68ed084621eb377ef422576c6e9
Great_Guy96 <greatguydev@gmail.com>
2022-08-16 21:11:25 +0000

    Add Catering Page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,21 @@
-import {Link} from '@solidjs/router'
-import styles from "./Card.module.scss";
+import {NavLink} from '@solidjs/router'
+import styles from "./Footer.module.scss";
 
 export default function Footer(props?: {children?: any, title?: string}) {
   return (
-    <div class={styles.card}>
-      <h3>{props.title}</h3>
-      <p>{props.children}</p>
-      <img src="/images/decorations/card-outline-top.svg" class={`${styles.outline} ${styles.top}`} />
-      <img src="/images/decorations/card-outline-bottom.svg" class={`${styles.outline} ${styles.bottom}`} />
+    <div class={styles.footer}>
+			<img src="/images/decorations/footer-decoration.svg" class={styles.decoration} draggable={false} />
+      <div class={styles.links}>
+				<div class={styles.left}>
+					<NavLink href="/" activeClass={styles.selectedTab} end={true} draggable={false}>Home</NavLink>
+          <NavLink href="/catering" activeClass={styles.selectedTab} draggable={false}>Catering</NavLink>
+				</div>
+				<img src="/images/logo.svg" draggable={false} />
+				<div class={styles.right}>
+					<NavLink href="/farmers-market" activeClass={styles.selectedTab} draggable={false}>Farmers' Market</NavLink>
+          <NavLink href="/contact" activeClass={styles.selectedTab} draggable={false}>Contact</NavLink>
+				</div>
+			</div>
     </div>
   );
 }
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,25 +1,77 @@
-import {NavLink} from '@solidjs/router'
-import styles from "./Layout.module.scss";
+import { NavLink } from '@solidjs/router';
+import Footer from '../Footer';
+import styles from './Layout.module.scss';
 
-export default function Layout(props?: {children?: any;}) {
+let previousLayout = undefined;
+
+export default function Layout(props?: { children?: any; home?: any }) {
+  if (props.home === undefined) props.home = false;
+  let logoTransition = props.home ? styles.bigLogo : styles.smallLogo;
+  if (previousLayout === undefined) {
+    console.log('First rendering of layout');
+  } else {
+    console.log(`Going from layout ${previousLayout} to layout ${props.home}`);
+    if (previousLayout === true && props.home === false) {
+      logoTransition = styles.toSmallLogo;
+    } else if (previousLayout === false && props.home === true) {
+      logoTransition = styles.toBigLogo;
+    }
+  }
+  previousLayout = props.home;
+
   return (
     <div class={styles.root}>
       <div class={styles.head}>
-        <img src="/images/decorations/nav-top.svg" class={styles.decoration} />
-        <img src="/images/full-logo.svg" class={styles.logo} />
+        <img
+          src="/images/decorations/nav-top.svg"
+          class={`${styles.decoration} ${logoTransition}`}
+          draggable={false}
+        />
+        <img
+          src="/images/full-logo.svg"
+          class={`${logoTransition} ${styles.logo}`}
+          draggable={false}
+        />
         <nav>
-          <NavLink href="/" activeClass={styles.selectedTab} end={true}>Home</NavLink>
-          <NavLink href="/catering" activeClass={styles.selectedTab}>Catering</NavLink>
-          <NavLink href="/farmers-market" activeClass={styles.selectedTab}>Farmers' Market</NavLink>
-          <NavLink href="/contact" activeClass={styles.selectedTab}>Contact</NavLink>
+          <NavLink
+            href="/"
+            activeClass={styles.selectedTab}
+            end={true}
+            draggable={false}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            href="/catering"
+            activeClass={styles.selectedTab}
+            draggable={false}
+          >
+            Catering
+          </NavLink>
+          <NavLink
+            href="/farmers-market"
+            activeClass={styles.selectedTab}
+            draggable={false}
+          >
+            Farmers' Market
+          </NavLink>
+          <NavLink
+            href="/contact"
+            activeClass={styles.selectedTab}
+            draggable={false}
+          >
+            Contact
+          </NavLink>
         </nav>
-				<div/>
+        <div />
         <img
           src="/images/decorations/nav-bottom.svg"
           class={styles.decoration}
+          draggable={false}
         />
       </div>
       <main class={styles.main}>{props.children}</main>
+      <Footer />
     </div>
   );
 }
